353d351f1bd958c0eccfe84b7718a15257654b7a
prisam1 <110589557+prisam1@users.noreply.github.com>
2023-10-10 22:14:42 +0530

    Update urlControllers.js

--- src/controller/urlControllers.js
+++ src/controller/urlControllers.js
@@ -34,7 +34,7 @@ const urlShorter = async function (req, res) {
              
      
         let urlCode = (shortid.generate()).toLowerCase();
-        let baseUrl = "https://localhost:5001/"
+        let baseUrl = "https://tiny.url/"
         let shortUrl = baseUrl + urlCode;
 
         let savedData = await urlModel.create({ urlCode: urlCode, longUrl: longUrl, shortUrl: shortUrl })

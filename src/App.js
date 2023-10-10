93cedc45e105f42956ec3c5aaa30d19fcd958ce9
Pritam <pritamsamaddar840@gmail.com>
2023-10-10 13:01:29 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
+  const [longUrl, setLongUrl] = useState('')
+  const [shortUrl, setShortUrl] = useState('')
+  const [message, setMessage] = useState('')
+  const [error, setError] = useState('')
+  const [urlCode, setUrlCode] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const response = await fetch("/url/shorten", {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ longUrl }),
+      })
+     
+      const data = await response.json()
+
+     console.log(data) 
+      if (data.status==="false") {
+
+        setMessage(data.message)
+        setShortUrl('')
+        
+      } else {
+
+        setMessage(data.message)
+        setShortUrl(data.data.shortUrl)
+        setUrlCode(data.data.urlCode)
+      }
+    } catch (error) {
+      console.error('Error:', error)
+    }
+  }
+
+  const handleRedirect = async () => {
+    try {
+      const response = await fetch("/get", {
+        method: 'GET',
+      })
+
+      const data = await response.json()
+
+      if (response.ok) {
+        window.location.href = data.data.shortUrl
+      } else {
+        const errorData = await response.json()
+        setMessage(errorData.message)
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      setError('An error occurred while redirecting.')
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>URL Shortener</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a long URL"
+          value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)}
+          
+        />
+        <button type="submit">Shorten</button>
+      </form>
+
+      {error && (
+        <div className="error-message">
+          <p>Error: {error}</p>
+        </div>
+      )}
+   
+    {!shortUrl && (
+        <div className="error-message">
+          <p>{message}</p>
+        </div>
+      )}
+    
+
+      {shortUrl && (
+        <div>
+          <h3>{message}</h3>
+          <p>Shortened URL: <a href={shortUrl} target="_blank" rel="noopener noreferrer">{shortUrl}</a></p>
+          <button onClick={handleRedirect}>Go to Short URL</button>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
